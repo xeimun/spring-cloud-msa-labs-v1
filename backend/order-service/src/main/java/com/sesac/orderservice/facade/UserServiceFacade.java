2c26219bf65cd2ba5c55c541a8863cff8a618e01
xeimun <hinote444@naver.com>
2025-08-12 12:21:33 +0900

    feat: retry 패턴 적용

--- backend/order-service/src/main/java/com/sesac/orderservice/facade/UserServiceFacade.java
+++ backend/order-service/src/main/java/com/sesac/orderservice/facade/UserServiceFacade.java
@@ -3,6 +3,7 @@ package com.sesac.orderservice.facade;
 import com.sesac.orderservice.client.UserServiceClient;
 import com.sesac.orderservice.client.dto.UserDto;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,6 +16,7 @@ public class UserServiceFacade {
     private final UserServiceClient userServiceClient;
 
     @CircuitBreaker(name = "user-service", fallbackMethod = "getUserFallback")
+    @Retry(name = "user-service")
     public UserDto getUserWithFallback(Long userId) {
         log.info("User service 호출 시도 - userId = {}", userId);
         return userServiceClient.getUserById(userId);
